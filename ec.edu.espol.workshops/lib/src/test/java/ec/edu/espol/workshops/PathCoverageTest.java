2ec83a3534a1528ec79199e553edb1a076fd0d09
karinaortega1507 <krortega@espol.edu.ec>
2022-01-03 14:03:14 -0500

    Creaión de la clase PathCoverageTest

---
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/PathCoverageTest.java
@@ -0,0 +1,14 @@
+package ec.edu.espol.workshops;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PathCoverageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
